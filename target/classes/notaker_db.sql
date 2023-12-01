da80a4c2e2e38062840b09ab48f167c958286872
Shemuel2060 <2060newty@gmail.com>
2023-12-01 10:26:46 +0300

    db script starter

---
+++ target/classes/notaker_db.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS arola_cornellnotaker;
+
+CREATE DATABASE arola_cornellnotaker;
+
+USE arola_cornellnotaker; 
+
+-- CRUD OPERATIONS
+
+
+
+-- Error Code: 1044. Access denied for user 'arola'@'localhost' to database 'arola_cornellnotaker'
+
+
+
+
+
